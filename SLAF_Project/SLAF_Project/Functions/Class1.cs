42fedde8bb3299d9354d748e50f41ffd3e5d63ec
Madhawi1 <demelmadavi@yahoo.com>
2013-09-24 22:05:45 +0530

    A-Team All rights reserved

---
+++ SLAF_Project/SLAF_Project/Functions/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SLAF_Project.Functions
+{
+    class Class1
+    {
+    }
+}
